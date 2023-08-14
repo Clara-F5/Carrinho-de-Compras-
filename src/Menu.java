1b7e9758cd0fbc9b9d84f0b5f61e69cc38b7b740
Clara <clara.almeida@routerlink.com.br>
2023-08-14 14:49:33 -0300

    Versão Corrigida e Otimizada

--- src/Menu.java
+++ src/Menu.java
@@ -1,60 +1,45 @@
-import modelos.Produto;
+import modelos.CartaoDeCredito;
+import modelos.Compra;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        List<Produto> carrinho = new ArrayList<>();
-        double limite = 0;
-        int escolha = 0;
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o limite do cartão: ");
+        double limite = leitura.nextDouble();
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
 
+        int sair = 1;
+        while(sair != 0) {
+            System.out.println("Digite a descrição da compra:");
+            String descricao = leitura.next();
 
-        System.out.println("*****************************");
-        System.out.println("Programa de carrinho");
-        System.out.println("*****************************");
+            System.out.println("Digite o valor da compra:");
+            double valor = leitura.nextDouble();
 
-        System.out.println("Digite o limite de seu cartão");
-        limite = sc.nextDouble();
+            Compra compra = new Compra(descricao, valor);
+            boolean compraRealizada = cartao.lancaCompra(compra);
 
-        do{
-
-            System.out.println("Deseja adicionar um novo produto?");
-            System.out.println("1 - para Sim");
-            System.out.println("0 - para Não");
-            escolha = sc.nextInt();
-
-            if(escolha == 1){
-               Produto produto = new Produto();
-
-                System.out.println("Digite a descrição da compra");
-                produto.setDescricao(sc.next());
-
-                System.out.println("Digite o valor da compra");
-                produto.setValor(Double.parseDouble(sc.next()));
-
-
-                if(produto.getValor() > limite){
-                    System.out.println("Limite insuficiente para compra");
-                    System.out.println("Programa Encerrado por falta de limite");
-                    escolha = 0;
-                }
-
-                carrinho.add(produto);
-                limite -= produto.getValor();
-                System.out.println("Saldo atual: "+limite);
+            if (compraRealizada) {
+                System.out.println("Compra realizada!");
+                System.out.println("Digite 0 para sair ou 1 para continuar");
+                sair = leitura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                sair = 0;
             }
+        }
 
-        }while (escolha!=0);
+        System.out.println("***********************");
+        System.out.println("COMPRAS REALIZADAS:\n");
 
-        System.out.println("FATURA");
-        Collections.sort(carrinho);
-        System.out.println(carrinho);
+        for (Compra c : cartao.getCompras()) {
+            System.out.println(c.getDescricao() + " - " +c.getValor());
+        }
+        System.out.println("\n***********************");
 
-
+        System.out.println("\nSaldo do cartão: " +cartao.getSaldo());
     }
 }
